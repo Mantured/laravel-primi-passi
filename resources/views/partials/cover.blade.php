d9cdc4ea9565170e148d64af15e5b87968ccfbf2
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-05-03 15:25:22 +0200

    url solution for link

---
+++ resources/views/partials/cover.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>im_a_link</title>
+
+    <style>
+        main{
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+        }
+        .container{
+            margin: 3rem auto;
+            width: 60vw;
+            
+        }
+    </style>
+
+</head>
+<body>
+    <main>
+        <div class="container">
+            <img src="https://totally80s.com/sites/totally80s.rock.tools/files/styles/site_width_image/public/2020-03/metallica-master-puppets.jpg?itok=m3C1P0SF" alt="master_of_puppets_cover_image">
+        </div>
+    </main>
+</body>
+</html>
